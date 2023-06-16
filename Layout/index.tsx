2316cf60debd21db68ff19a277b4244bae4f5973
Josh Merlino <joshua.merlino@gmail.com>
2023-06-16 01:39:11 -0400

    Add layout components

---
+++ Layout/index.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@util/cn";
+import { HTMLAttributes } from "react";
+
+export function Container({ children, className, ...props }: HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div className={cn("max-w-7xl mx-auto w-full flex flex-col", className)} {...props}>
+			{children}
+		</div>
+	);
+}

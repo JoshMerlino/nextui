6b539025f0b78719c1af65be35721d817d717d03
Josh Merlino <joshua.merlino@gmail.com>
2023-09-02 16:52:58 -0400

    make icons better

--- Icon/index.tsx
+++ Icon/index.tsx
@@ -6,8 +6,8 @@ import { cn } from "../util";
 
 export function Icon({ icon: Icon, className, ...props }: { icon: IconType; } & HTMLAttributes<HTMLButtonElement>) {
 	return (
-		<button className={ cn("relative w-12 overflow-hidden text-2xl text-center rounded-full aspect-square transition-colors duration-75 hover:bg-white/10 focus:bg-white/10 outline-0", className) } { ...props }>
-			<Ripple emitFromCenter />
+		<button className={ cn("relative w-12 overflow-hidden text-2xl text-center rounded-full aspect-square transition-colors duration-75 hover:bg-black/10 focus:bg-black/10 outline-0 dark:hover:bg-white/10 dark:focus:bg-white/10", className) } { ...props }>
+			<Ripple className="bg-black/20 dark:bg-white/20" emitFromCenter />
 			<Icon className="mx-auto" />
 		</button>
 	);

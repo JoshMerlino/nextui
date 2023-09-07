167fb1265ac85e112e43b257aa37f58daa318d91
Josh Merlino <joshua.merlino@gmail.com>
2023-09-07 19:40:27 -0400

    make icon type button

--- Icon/index.tsx
+++ Icon/index.tsx
@@ -6,7 +6,7 @@ import { cn } from "../util";
 
 export function Icon({ icon: Icon, className, ...props }: { icon: IconType; } & HTMLAttributes<HTMLButtonElement>) {
 	return (
-		<button className={ cn("relative w-12 overflow-hidden text-2xl text-center rounded-full aspect-square transition-colors duration-75 hover:bg-black/10 focus:bg-black/10 outline-0 dark:hover:bg-white/10 dark:focus:bg-white/10", className) } { ...props }>
+		<button className={ cn("relative w-12 overflow-hidden text-2xl text-center rounded-full aspect-square transition-colors duration-75 hover:bg-black/10 focus:bg-black/10 outline-0 dark:hover:bg-white/10 dark:focus:bg-white/10", className) } { ...props } type="button">
 			<Ripple className="bg-black/20 dark:bg-white/20" emitFromCenter />
 			<Icon className="mx-auto" />
 		</button>

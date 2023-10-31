42b87eff49a499a320aefb2698ed9ba2aaa584a0
Josh Merlino <joshua.merlino@gmail.com>
2023-10-31 18:44:42 -0400

    add tooltip element support

--- Tooltip/index.tsx
+++ Tooltip/index.tsx
@@ -7,7 +7,12 @@ interface Props {
 	/**
 	 * The tooltip content
 	 */
-	tooltip: ReactNode;
+	tooltip?: ReactNode;
+
+	/**
+	 * The tooltip itsself
+	 */
+	element?: ReactNode;
 
 	/**
 	 * The anchor position of the tooltip
@@ -23,7 +28,9 @@ interface Props {
 	
 }
 
-export function Tooltip({ tooltip, anchor = "top", children, className }: PropsWithChildren<Props>) {
+export function Tooltip({ tooltip, anchor = "top", children, className, element }: PropsWithChildren<Props>) {
+
+	if (!tooltip && !element) throw new Error("You must provide either a tooltip or an element to render as the tooltip");
 
 	// Record of classnames to apply based on props
 	const classes: ClassValue[] = [
@@ -40,7 +47,7 @@ export function Tooltip({ tooltip, anchor = "top", children, className }: PropsW
 	const tooltipClasses: ClassValue[] = [
 	
 		// Base classnames
-		"absolute inline-flex items-center h-6 px-2 text-xs font-medium text-white transition-all scale-0 rounded-md opacity-0 pointer-events-none select-none bg-neutral-500 dark:bg-neutral-700 group-hover/tooltip:opacity-100 group-hover/tooltip:scale-100 whitespace-nowrap z-[100]",
+		"absolute inline-flex items-center transition-all scale-0 opacity-0 group-hover/tooltip:opacity-100 group-hover/tooltip:scale-100 z-[100]",
 	
 		// Anchor classes
 		{
@@ -54,7 +61,7 @@ export function Tooltip({ tooltip, anchor = "top", children, className }: PropsW
 	return (
 		<div className={ cn(classes) }>
 			{children}
-			<div className={ cn(tooltipClasses) }>{tooltip}</div>
+			{element ? <div className={ cn(tooltipClasses) }>{element}</div> : <div className={ cn(tooltipClasses, "h-6 px-2 text-xs font-medium text-white rounded-md select-none bg-neutral-500 dark:bg-neutral-700 whitespace-nowrap pointer-events-none") }>{tooltip}</div>}
 		</div>
 	);
 }

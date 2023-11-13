1f31ebc203552b27739c118d4d7cfedbdbdfe70b
Josh Merlino <joshua.merlino@gmail.com>
2023-11-13 17:28:07 -0500

    add ExpandableGroup and AdjustableHeight to nextui

---
+++ AdjustableHeight/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { DependencyList, PropsWithChildren, useEffect, useRef } from "react";
+
+export function AdjustableHeight({ children, deps = []}: PropsWithChildren<{ deps?: DependencyList; }>) {
+	const wrapperRef = useRef<HTMLDivElement>(null);
+
+	useEffect(function() {
+		const wrapper = wrapperRef.current;
+
+		function resize() {
+			if (!wrapper) return;
+
+			// Get the height of all the contents
+			const height = Array.from(wrapper.children).reduce((height, child) => height + child.getBoundingClientRect().height, 0);
+
+			// Set the height of the wrapper
+			wrapper.style.height = `${ height }px`;
+
+		}
+
+		requestAnimationFrame(resize);
+		setTimeout(resize, 100);
+		window.addEventListener("resize", resize);
+		return () => window.removeEventListener("resize", resize);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, deps);
+
+	return <div className="transition-[height] bg-inherit" ref={ wrapperRef }>{children}</div>;
+}
---
+++ ExpandableGroup/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { AdjustableHeight } from "nextui/AdjustableHeight";
+import { useEvent } from "nextui/hooks";
+import { PropsWithChildren, useRef, useState } from "react";
+import { ToggleButton } from "./ToggleButton";
+
+export default function ExpandableGroup({
+	button: Button = ToggleButton,
+	children,
+	defaultHeight = 500,
+	gap = 16,
+	shadowSize = 64,
+}: PropsWithChildren<{
+	button?: (props: { isExpanded: boolean; onClick: () => void; }) => JSX.Element,
+	defaultHeight?: number,
+	gap?: number,
+	shadowSize?: number,
+}>) {
+	const [ height, setHeight ] = useState<number | undefined>(defaultHeight);
+	const isExpanded = height !== defaultHeight;
+	const ref = useRef<HTMLDivElement>(null);
+
+	function getNaturalHeight() {
+		if (!ref.current) return defaultHeight;
+		const height = Array.from(ref.current.children).reduce((height, child) => height + child.getBoundingClientRect().height, 0);
+		return height;
+	}
+
+	function toggle() {
+		if (!ref.current) return;
+		const naturalHeight = getNaturalHeight();
+		if (height === naturalHeight) setHeight(defaultHeight);
+		else setHeight(naturalHeight);
+	}
+
+	useEvent("resize", function() {
+		if (!ref.current) return;
+		if (height === defaultHeight) return;
+		const naturalHeight = getNaturalHeight();
+		setHeight(naturalHeight);
+	});
+
+	return (
+		<div className="flex flex-col bg-inherit isolate">
+			<div className="overflow-y-hidden relative bg-inherit mask transition-[padding]" style={{ paddingBottom: isExpanded ? shadowSize : 0 }}>
+				<style>{`.mask{
+				-webkit-mask-image: -webkit-linear-gradient(top,black 0%, black calc(100% - ${ shadowSize }px), transparent 100%);
+				mask-image: linear-gradient(to top,black 0%, black calc(100% - ${ shadowSize }px), transparent 100%);
+				}`}</style>
+				<AdjustableHeight deps={ [ height ] }>
+					<div className="-mb-4 pb-4" ref={ ref } style={{ height }}>{children}</div>
+				</AdjustableHeight>
+			</div>
+			<div className="flex justify-center transition-[margin,padding] z-10" style={{ marginTop: isExpanded ? -shadowSize : 0, paddingTop: isExpanded ? gap : 0 }}>
+				<Button
+					isExpanded={ isExpanded }
+					onClick={ toggle } />
+			</div>
+		</div>
+	);
+}

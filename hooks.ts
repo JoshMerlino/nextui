697a72c4ffa3bec04ff43ffaf03a9c212c50a8e8
Josh Merlino <joshua.merlino@gmail.com>
2023-11-13 00:10:01 -0500

    add useResize hook

--- hooks.ts
+++ hooks.ts
@@ -1,10 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import { DependencyList, useEffect } from "react";
 
 export function useEvent<K extends keyof WindowEventMap>(event: K, handler: (this: Window, ev: WindowEventMap[K]) => any) {
 	useEffect(function() {
 		window.addEventListener(event, handler);
 		return () => window.removeEventListener(event, handler);
 	}, [ event, handler ]);
+}
+
+export function useResize(handler: () => any, additionalDeps?: DependencyList) {
+	useEffect(function() {
+		handler();
+		window.addEventListener("resize", handler);
+		return () => window.removeEventListener("resize", handler);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ handler, ...additionalDeps ?? [] ]);
 }

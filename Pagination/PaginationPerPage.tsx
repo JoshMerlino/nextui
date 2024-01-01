eace7098031393d51d34a7f04222cbde25b9d058
Josh Merlino <joshua.merlino@gmail.com>
2024-01-01 01:38:22 -0500

    Refactor PaginationPerPage component

--- Pagination/PaginationPerPage.tsx
+++ Pagination/PaginationPerPage.tsx
@@ -6,16 +6,14 @@ import { usePagination } from ".";
 export function PaginationPerPage({ amounts = [ 10, 25, 50, 100 ]}: Partial<{ amounts: number[] }>) {
 	const { perPage: [ perPage, setPerPage ], cursor: [ cursor, setCursor ], total: [ total ] } = usePagination();
 	return (
-		<div className="my-2 flex items-center gap-2 text-gray-900 dark:text-white font-medium text-sm">
+		<div className="flex items-center gap-2 my-2 text-sm font-medium text-gray-900 dark:text-white">
 			<p>Show</p>
 			<InputField
 				onChange={ e => [
 					setPerPage(parseInt(e.target.value)),
 					setCursor(Math.min(cursor, Math.ceil(total / parseInt(e.target.value)) * parseInt(e.target.value)))
 				] }
-				options={ amounts
-					.filter((a, b, c) => total > a || total + perPage > c[b - 1])
-					.map(a => a + "") }
+				options={ amounts.map(a => a.toString()) }
 				style={{ width: "32px" }}
 				type="select"
 				value={ perPage.toString() }

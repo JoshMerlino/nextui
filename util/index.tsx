0c57cec7b35983cd62884b3360a52b6f493aaa56
Josh Merlino <joshua.merlino@gmail.com>
2024-02-25 02:08:53 -0500

    Add rgba function to util/index.tsx

--- util/index.tsx
+++ util/index.tsx
@@ -11,3 +11,11 @@ export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
  * Create a style tag with the given CSS.
  */
 export const css = (strings: TemplateStringsArray, ...values: Array<string | number>) => <style>{ String.raw(strings, ...values) }</style>;
+
+export function rgba(hex: string, alpha: string | number) {
+	if (hex.length === 4) hex = hex.replace(/./g, "$&$&");
+	const [ r, g, b ] = hex.match(/\w\w/g)!.map(c => parseInt(c, 16));
+	if (typeof alpha === "string") alpha = Number(alpha);
+	if (alpha > 1) alpha = alpha / 100;
+	return `rgba(${ r }, ${ g }, ${ b }, ${ alpha })`;
+}

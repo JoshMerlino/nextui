a0c75849d8b86d17015baa3b96611868b9e88a7f
Josh Merlino <joshua.merlino@gmail.com>
2024-03-16 13:14:59 -0400

    Refactor Icon component to improve accessibility and styling

--- Icon/index.tsx
+++ Icon/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { HTMLAttributes } from "react";
 import { IconType } from "react-icons";
 import { Ripple } from "../Ripple";
@@ -6,9 +7,16 @@ import { cn } from "../util";
 
 export function Icon({ icon: Icon, className, ...props }: { icon: IconType; } & HTMLAttributes<HTMLButtonElement>) {
 	return (
-		<button className={ cn("relative w-12 overflow-hidden text-2xl text-center rounded-full aspect-square transition-colors duration-75 hover:bg-black/10 focus:bg-black/10 outline-0 dark:hover:bg-white/10 dark:focus:bg-white/10", className) } { ...props } type="button">
+		<button { ...props } 
+		className={ cn([
+			"rounded-full select-none p-0.5 shrink-0 overflow-hidden w-10 aspect-square relative",
+			"onClick" in props && "hover:bg-black/5 dark:hover:bg-white/5 hover:ring-2 hover:ring-black/5 dark:hover:ring-white/5 focus:ring-2 active:ring-2 ring-gray-500/50 active:ring-gray-500 dark:active:ring-gray-500 focus:ring-gray-500/50 dark:focus:ring-gray-500/50 focus:outline-0",
+			className
+		]) } 
+		tabIndex={ "onClick" in props ? 0 : undefined }
+		type="button">
 			<Ripple className="bg-black/20 dark:bg-white/20" emitFromCenter />
-			<Icon className="mx-auto" />
+			<Icon className="mx-auto text-2xl" />
 		</button>
 	);
 }

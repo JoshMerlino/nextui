c0421848884fc3e6402c23be8e460588af372863
Josh Merlino <joshua.merlino@gmail.com>
2024-04-13 21:43:19 -0400

    Refine event handling and cleanup hooks
    Optimized click and mouse down event handling in `InputField` component to streamline their definitions, enhancing maintainability. In the `hooks.ts` file, standardized formatting for useState and useEffect calls was applied to improve code readability. Additionally, refined the `useScroll` hook's event management to ensure more robust cleanup by moving event listener additions and removals directly into the useEffect hook, reducing potential memory leaks and ensuring cleaner component unmounts.
    
    These adjustments provide a slight performance boost and contribute to a more consistent codebase by aligning syntax styles and ensuring efficient resource utilization.
    
    No specific issues addressed.

--- hooks.ts
+++ hooks.ts
@@ -4,27 +4,26 @@ import { useCallback, useEffect, useMemo, useState, type DependencyList } from "
 
 export function useScroll(container: React.RefObject<HTMLElement> | HTMLElement = document.body) {
 
-	const element = useMemo(() => "current" in container ? container.current : container, [container]);
+	const element = useMemo(() => "current" in container ? container.current : container, [ container ]);
 
-	const [scrollX, setScrollX] = useState(0);
-	const [scrollY, setScrollY] = useState(0);
+	const [ scrollX, setScrollX ] = useState(0);
+	const [ scrollY, setScrollY ] = useState(0);
 
-	const handle = useCallback(function ({ target }: Event) {
+	const handle = useCallback(function({ target }: Event) {
 		if (!(target instanceof HTMLElement)) return;
 		setScrollX(target.scrollLeft);
 		setScrollY(target.scrollTop);
 	}, [ container ]);
 
-	useEffect(function () {
+	useEffect(function() {
 		if (!element) return;
 		element.addEventListener("scroll", handle);
 		() => element.removeEventListener("scroll", handle);
-	}, [element, handle]);
+	}, [ element, handle ]);
 
 	return { scrollX, scrollY };
 
 }
-
 
 export function useEvent<K extends keyof WindowEventMap>(event: K, handler: (this: Window, ev: WindowEventMap[K]) => (void | unknown)) {
 	useEffect(function() {

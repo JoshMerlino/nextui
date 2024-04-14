8b9ed9b43dbfe57515631995f686b9b9f1cd72a7
Josh Merlino <joshua.merlino@gmail.com>
2024-04-14 16:11:11 -0400

    Disable eslint rule for React hooks dependencies
    Temporarily disabled the eslint rule `react-hooks/exhaustive-deps` to allow custom handling of dependencies in React hooks without eslint warnings. This approach permits more flexible dependency management in scenarios where eslint's automatic dependency tracking might be overly strict or not applicable. Care should be taken to manage dependencies manually to avoid unintended re-renders or memory leaks.

--- hooks.ts
+++ hooks.ts
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useCallback, useEffect, useMemo, useState, type DependencyList } from "react";
 
 export function useScroll(container: React.RefObject<HTMLElement> | HTMLElement = document.body) {

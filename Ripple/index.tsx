6ed8131ab468ca7e408fa7a9ebcfa49ff95ba6b3
JoshMerlino <joshua.merlino@gmail.com>
2024-05-13 07:38:49 -0400

    Refactor Ripple component to add draggable attribute

--- Ripple/index.tsx
+++ Ripple/index.tsx
@@ -114,5 +114,5 @@ export function Ripple({ emitFromCenter, className, disabled }: Partial<Props>) 
 
 	if (disabled) return null;
 
-	return <div draggable="false" className="absolute inset-0 z-50 opacity-30 ripple" ref={ ref } />;
+	return <div className="absolute inset-0 z-50 opacity-30 ripple" draggable="false" ref={ ref } />;
 }

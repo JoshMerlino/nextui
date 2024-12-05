c8aed0b05411559ff375fc7b0837c5a15378705a
JoshMerlino <joshua.merlino@gmail.com>
2024-12-04 19:07:48 -0500

    Add AdjustableHeight component; implement dynamic height adjustment based on content size and window resize events

---
+++ components/AdjustableHeight.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { DependencyList, PropsWithChildren, useCallback, useEffect, useRef } from "react";
+
+export function AdjustableHeight({ children, deps = []}: PropsWithChildren<Partial<{
+
+    /**
+     * Additional dependencies to watch for changes
+     * @default []
+     */
+    deps?: DependencyList;
+
+}>>) {
+
+	const wrapperRef = useRef<HTMLDivElement>(null);
+    
+	const resize = useCallback(function() {
+		
+		const wrapper = wrapperRef.current;
+		if (!wrapper) return;
+
+		// Get the height of all the contents
+		const height = Array.from(wrapper.children).reduce((height, child) => height + child.getBoundingClientRect().height, 0);
+
+		// Set the height of the wrapper
+		wrapper.style.height = `${ height }px`;
+        
+	}, []);
+
+	useEffect(function() {
+		requestAnimationFrame(resize);
+		setTimeout(resize, 100);
+		window.addEventListener("resize", resize);
+		return () => window.removeEventListener("resize", resize);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ resize, ...deps ]);
+
+	return (
+		<div
+			className="transition-[height] bg-inherit"
+			ref={ wrapperRef }>
+			{ children }
+		</div>
+	);
+}

f18850c80bf463228106dfebee6dea56297d1a86
Josh Merlino <joshua.merlino@gmail.com>
2025-01-20 10:59:47 -0500

    Add Toolbar and ToolbarShell components; implement dynamic shadow effect on scroll and context management for improved layout control

---
+++ components/Toolbar.tsx
@@ -0,0 +1,67 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { merge } from "lodash";
+import { cn } from "nextui/util";
+import { use, type HTMLAttributes, type ReactNode } from "react";
+import { Container } from "./Container";
+import { ToolbarShellContext } from "./ToolbarShell";
+
+export const classes = {
+	toolbar: cva("min-h-16 transition-[box-shadow,border-color] border-b", {
+		variants: {
+			raised: {
+				true: "shadow-md border-transparent dark:shadow-black/30",
+				false: "border-gray-200 dark:border-gray-800"
+			},
+			variant: {
+				glass: "backdrop-blur bg-gray-100/60 dark:bg-gray-900/60"
+			}
+		},
+		defaultVariants: {
+			variant: "glass"
+		}
+	})
+};
+
+export function Toolbar({ children, className, contained = true, icon, ...props }: HTMLAttributes<HTMLElement> & VariantProps<typeof classes.toolbar> & Partial<{
+
+	/**
+	 * Whether the toolbar has a shadow.
+	 * The raised value can also be controlled by the ToolbarShell component.
+	 * @default false
+	 */
+	raised: boolean;
+
+	/**
+	 * The variant of the toolbar.
+	 * @default glass
+	 */
+	variant: "glass";
+
+	/**
+	 * Whether the toolbar is contained within a container.
+	 * @default true
+	 */
+	contained: boolean;
+
+	/**
+	 * The primary icon to display on the toolbar.
+	 */
+	icon: ReactNode;
+
+}>) {
+	const raised = use(ToolbarShellContext);
+	return (
+		<header
+			{ ...props }
+			className={ cn(classes.toolbar(merge(props, { raised }) as VariantProps<typeof classes.toolbar>), className) }>
+			
+			<div className="flex items-center mx-4 gap-4 h-16">
+				{ icon }
+				<Container className={ cn(contained || "max-w-full") }>
+					{ children }
+				</Container>
+			</div>
+		</header>
+	);
+}
+
---
+++ components/ToolbarShell.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { createContext, HTMLAttributes, ReactNode, useEffect, useRef, useState } from "react";
+import { cn } from "../util";
+
+export const ToolbarShellContext = createContext(false);
+
+export function ToolbarShell({ children, className, toolbar, wrapperProps, ...props }: {
+
+	/**
+	 * The toolbar to render
+	 */
+	toolbar: ReactNode;
+
+} & HTMLAttributes<HTMLDivElement> & { wrapperProps?: HTMLAttributes<HTMLDivElement> }) {
+
+	// Get a ref to the content
+	const ref = useRef<HTMLDivElement>(null);
+	const toolbarRef = useRef<HTMLDivElement>(null);
+	const [ raised, setRaised ] = useState(false);
+
+	useEffect(function() {
+		const wrapper = ref.current;
+		if (!wrapper) return;
+
+		// Get the controller
+		const controller = new AbortController();
+
+		// Add the event listener
+		wrapper.addEventListener("scroll", function() {
+			if (!wrapper) return;
+			if (wrapper.scrollTop > 0) setRaised(true);
+			else if (raised) setRaised(false);
+		}, { signal: controller.signal });
+		return () => controller.abort();
+
+	}, [ raised, setRaised ]);
+
+	return (
+		<div className={ cn("absolute inset-0 flex flex-col min-h-full overflow-hidden overflow-y-auto bg-inherit isolate", wrapperProps?.className) } ref={ ref } { ...wrapperProps }>
+			<ToolbarShellContext value={ raised }>
+				<div className="sticky isolate left-0 right-0 top-0 z-30" ref={ toolbarRef }>{ toolbar }</div>
+			</ToolbarShellContext>
+			<div className={ cn("grow overflow-visible bg-inherit flex flex-col relative", className) } { ...props }>{ children }</div>
+		</div>
+	);
+}

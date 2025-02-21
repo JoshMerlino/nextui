9d20ee5625ade2fe010a7879884c615e68c94fc8
JoshMerlino <joshua.merlino@gmail.com>
2025-02-21 16:23:59 -0500

    Refactor Button component to support multiple color options and improve className handling

--- components/Button.tsx
+++ components/Button.tsx
@@ -54,143 +54,158 @@ export const classes = {
 		
 			{
 				variant: "raised",
-				color: "primary",
+				color: [ "primary", "primary:pastel" ],
 				className: "text-white bg-primary-600 hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-800"
 			}, {
 				variant: "flat",
-				color: "primary",
-				className: "text-primary hover:bg-primary/10 focus:bg-primary/15 active:bg-primary/20 dark:text-primary dark:hover:bg-primary/10 dark:focus:bg-primary/15 dark:active:bg-primary/20"
+				color: [ "primary", "primary:pastel" ],
+				className: "text-primary hover:bg-primary/10 focus:bg-primary/15 active:bg-primary/20"
 			}, {
 				variant: "outlined",
-				color: "primary",
-				className: "text-primary hover:bg-primary/10 focus:bg-primary/15 active:bg-primary/20 dark:text-primary dark:hover:bg-primary/10 dark:focus:bg-primary/15 dark:active:bg-primary/20 border-primary/50"
-			},
-
-			{
+				color: [ "primary", "primary:pastel" ],
+				className: "text-primary hover:bg-primary/10 focus:bg-primary/15 active:bg-primary/20 border-primary/50"
+			}, {
 				variant: "raised",
 				color: "primary:pastel",
-				className: "text-white bg-primary-600 hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-800 dark:text-primary-950 dark:bg-primary-300 dark:hover:bg-primary-400 dark:focus:bg-primary-400 dark:active:bg-primary-500"
+				className: "dark:text-primary-950 dark:bg-primary-300 dark:hover:bg-primary-400 dark:focus:bg-primary-400 dark:active:bg-primary-500",
 			}, {
 				variant: "flat",
 				color: "primary:pastel",
-				className: "text-primary bg-primary dark:text-primary-300 dark:bg-primary-300 hover:bg-primary/10 focus:bg-primary/15 active:bg-primary/20 dark:hover:bg-primary-300/10 dark:focus:bg-primary-300/15 dark:active:bg-primary-300/20"
+				className: "dark:text-primary-300 dark:hover:bg-primary-300/10 dark:focus:bg-primary-300/15 dark:active:bg-primary-300/20",
 			}, {
 				variant: "outlined",
 				color: "primary:pastel",
-				className: "text-primary bg-primary border-primary/50 dark:text-primary-300 dark:bg-primary-300 dark:border-primary-300/50 hover:bg-primary/10 focus:bg-primary/15 active:bg-primary/20 dark:hover:bg-primary-300/10 dark:focus:bg-primary-300/15 dark:active:bg-primary-300/20"
+				className: "dark:text-primary-300 dark:hover:bg-primary-300/10 dark:focus:bg-primary-300/15 dark:active:bg-primary-300/20 dark:border-primary-300/50",
 			},
 			
 			{
 				variant: "raised",
-				color: "neutral",
-				className: "text-white bg-gray-600 hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-800 dark:text-gray-800 dark:bg-gray-300 dark:hover:bg-gray-200 dark:focus:bg-gray-200 dark:active:bg-gray-100"
-			}, {
-				variant: "flat",
-				color: "neutral",
-				className: "text-gray-800 bg-gray-800 dark:text-gray-200 dark:bg-gray-200"
-			}, {
-				variant: "outlined",
-				color: "neutral",
-				className: "text-gray-800 bg-gray-800 border-gray-800/50 dark:text-gray-200 dark:bg-gray-200 dark:border-gray-200/50"
-			},
-
-			{
-				variant: "raised",
-				color: "error",
+				color: [ "error", "error:pastel" ],
 				className: "text-white bg-error-600 hover:bg-error-700 focus:bg-error-700 active:bg-error-800"
 			}, {
 				variant: "flat",
-				color: "error",
-				className: "text-error hover:bg-error/10 focus:bg-error/15 active:bg-error/20 dark:text-error dark:hover:bg-error/10 dark:focus:bg-error/15 dark:active:bg-error/20"
+				color: [ "error", "error:pastel" ],
+				className: "text-error hover:bg-error/10 focus:bg-error/15 active:bg-error/20"
 			}, {
 				variant: "outlined",
-				color: "error",
-				className: "text-error hover:bg-error/10 focus:bg-error/15 active:bg-error/20 dark:text-error dark:hover:bg-error/10 dark:focus:bg-error/15 dark:active:bg-error/20 border-error/50"
-			},
-
-			{
+				color: [ "error", "error:pastel" ],
+				className: "text-error hover:bg-error/10 focus:bg-error/15 active:bg-error/20 border-error/50"
+			}, {
 				variant: "raised",
 				color: "error:pastel",
-				className: "text-white bg-error-600 hover:bg-error-700 focus:bg-error-700 active:bg-error-800 dark:text-error-950 dark:bg-error-300 dark:hover:bg-error-400 dark:focus:bg-error-400 dark:active:bg-error-500"
+				className: "dark:text-error-950 dark:bg-error-300 dark:hover:bg-error-400 dark:focus:bg-error-400 dark:active:bg-error-500",
 			}, {
 				variant: "flat",
 				color: "error:pastel",
-				className: "text-error bg-error dark:text-error-300 dark:bg-error-300 hover:bg-error/10 focus:bg-error/15 active:bg-error/20 dark:hover:bg-error-300/10 dark:focus:bg-error-300/15 dark:active:bg-error-300/20"
+				className: "dark:text-error-300 dark:hover:bg-error-300/10 dark:focus:bg-error-300/15 dark:active:bg-error-300/20",
 			}, {
 				variant: "outlined",
 				color: "error:pastel",
-				className: "text-error bg-error border-error/50 dark:text-error-300 dark:bg-error-300 dark:border-error-300/50 hover:bg-error/10 focus:bg-error/15 active:bg-error/20 dark:hover:bg-error-300/10 dark:focus:bg-error-300/15 dark:active:bg-error-300/20"
+				className: "dark:text-error-300 dark:hover:bg-error-300/10 dark:focus:bg-error-300/15 dark:active:bg-error-300/20 dark:border-error-300/50",
 			},
 			
 			{
 				variant: "raised",
-				color: "success",
-				className: "text-white bg-success-600 hover:bg-success-700 focus:bg-success-700 active:bg-success-800"
-			}, {
-				variant: "flat",
-				color: "success",
-				className: "text-success hover:bg-success/10 focus:bg-success/15 active:bg-success/20 dark:text-success dark:hover:bg-success/10 dark:focus:bg-success/15 dark:active:bg-success/20"
-			}, {
-				variant: "outlined",
-				color: "success",
-				className: "text-success hover:bg-success/10 focus:bg-success/15 active:bg-success/20 dark:text-success dark:hover:bg-success/10 dark:focus:bg-success/15 dark:active:bg-success/20 border-success/50"
-			},
-
-			{
-				variant: "raised",
-				color: "success:pastel",
-				className: "text-white bg-success-600 hover:bg-success-700 focus:bg-success-700 active:bg-success-800 dark:text-success-950 dark:bg-success-300 dark:hover:bg-success-400 dark:focus:bg-success-400 dark:active:bg-success-500"
-			}, {
-				variant: "flat",
-				color: "success:pastel",
-				className: "text-success bg-success dark:text-success-300 dark:bg-success-300 hover:bg-success/10 focus:bg-success/15 active:bg-success/20 dark:hover:bg-success-300/10 dark:focus:bg-success-300/15 dark:active:bg-success-300/20"
-			}, {
-				variant: "outlined",
-				color: "success:pastel",
-				className: "text-success bg-success border-success/50 dark:text-success-300 dark:bg-success-300 dark:border-success-300/50 hover:bg-success/10 focus:bg-success/15 active:bg-success/20 dark:hover:bg-success-300/10 dark:focus:bg-success-300/15 dark:active:bg-success-300/20"
-			},
-
-			{
-				variant: "raised",
-				color: "warning",
+				color: [ "warning", "warning:pastel" ],
 				className: "text-white bg-warning-600 hover:bg-warning-700 focus:bg-warning-700 active:bg-warning-800"
 			}, {
 				variant: "flat",
-				color: "warning",
-				className: "text-warning hover:bg-warning/10 focus:bg-warning/15 active:bg-warning/20 dark:text-warning dark:hover:bg-warning/10 dark:focus:bg-warning/15 dark:active:bg-warning/20"
+				color: [ "warning", "warning:pastel" ],
+				className: "text-warning hover:bg-warning/10 focus:bg-warning/15 active:bg-warning/20"
 			}, {
 				variant: "outlined",
-				color: "warning",
-				className: "text-warning hover:bg-warning/10 focus:bg-warning/15 active:bg-warning/20 dark:text-warning dark:hover:bg-warning/10 dark:focus:bg-warning/15 dark:active:bg-warning/20 border-warning/50"
-			},
-
-			{
+				color: [ "warning", "warning:pastel" ],
+				className: "text-warning hover:bg-warning/10 focus:bg-warning/15 active:bg-warning/20 border-warning/50"
+			}, {
 				variant: "raised",
 				color: "warning:pastel",
-				className: "text-white bg-warning-600 hover:bg-warning-700 focus:bg-warning-700 active:bg-warning-800 dark:text-warning-950 dark:bg-warning-300 dark:hover:bg-warning-400 dark:focus:bg-warning-400 dark:active:bg-warning-500"
+				className: "dark:text-warning-950 dark:bg-warning-300 dark:hover:bg-warning-400 dark:focus:bg-warning-400 dark:active:bg-warning-500",
 			}, {
 				variant: "flat",
 				color: "warning:pastel",
-				className: "text-warning bg-warning dark:text-warning-300 dark:bg-warning-300 hover:bg-warning/10 focus:bg-warning/15 active:bg-warning/20 dark:hover:bg-warning-300/10 dark:focus:bg-warning-300/15 dark:active:bg-warning-300/20"
+				className: "dark:text-warning-300 dark:hover:bg-warning-300/10 dark:focus:bg-warning-300/15 dark:active:bg-warning-300/20",
 			}, {
 				variant: "outlined",
 				color: "warning:pastel",
-				className: "text-warning bg-warning border-warning/50 dark:text-warning-300 dark:bg-warning-300 dark:border-warning-300/50 hover:bg-warning/10 focus:bg-warning/15 active:bg-warning/20 dark:hover:bg-warning-300/10 dark:focus:bg-warning-300/15 dark:active:bg-warning-300/20"
+				className: "dark:text-warning-300 dark:hover:bg-warning-300/10 dark:focus:bg-warning-300/15 dark:active:bg-warning-300/20 dark:border-warning-300/50",
 			},
 			
 			{
 				variant: "raised",
-				disabled: true,
-				className: "text-white bg-gray-500 shadow-none hover:shadow-none dark:text-white dark:bg-gray-500"
+				color: [ "success", "success:pastel" ],
+				className: "text-white bg-success-600 hover:bg-success-700 focus:bg-success-700 active:bg-success-800"
 			}, {
 				variant: "flat",
-				disabled: true,
-				className: "text-gray-700 dark:text-gray-200"
+				color: [ "success", "success:pastel" ],
+				className: "text-success hover:bg-success/10 focus:bg-success/15 active:bg-success/20"
 			}, {
 				variant: "outlined",
-				disabled: true,
-				className: "text-gray-700 dark:text-gray-200 border-gray-500/50 dark:border-gray-400/50"
+				color: [ "success", "success:pastel" ],
+				className: "text-success hover:bg-success/10 focus:bg-success/15 active:bg-success/20 border-success/50"
+			}, {
+				variant: "raised",
+				color: "success:pastel",
+				className: "dark:text-success-950 dark:bg-success-300 dark:hover:bg-success-400 dark:focus:bg-success-400 dark:active:bg-success-500",
+			}, {
+				variant: "flat",
+				color: "success:pastel",
+				className: "dark:text-success-300 dark:hover:bg-success-300/10 dark:focus:bg-success-300/15 dark:active:bg-success-300/20",
+			}, {
+				variant: "outlined",
+				color: "success:pastel",
+				className: "dark:text-success-300 dark:hover:bg-success-300/10 dark:focus:bg-success-300/15 dark:active:bg-success-300/20 dark:border-success-300/50",
+			},
+			
+			{
+				variant: "raised",
+				color: "neutral",
+				className: [
+					"text-white bg-neutral-600 hover:bg-neutral-700 focus:bg-neutral-700 active:bg-neutral-800",
+					"dark:text-neutral-950 dark:bg-neutral-300 dark:hover:bg-neutral-400 dark:focus:bg-neutral-400 dark:active:bg-neutral-500"
+				]
+			}, {
+				variant: "flat",
+				color: "neutral",
+				className: [
+					"text-neutral hover:bg-neutral/10 focus:bg-neutral/15 active:bg-neutral/20",
+					"dark:text-neutral-300 dark:hover:bg-neutral-300/10 dark:focus:bg-neutral-300/15 dark:active:bg-neutral-300/20"
+				]
+			}, {
+				variant: "outlined",
+				color: "neutral",
+				className: [
+					"text-neutral hover:bg-neutral/10 focus:bg-neutral/15 active:bg-neutral/20 border-neutral/50",
+					"dark:text-neutral-300 dark:hover:bg-neutral-300/10 dark:focus:bg-neutral-300/15 dark:active:bg-neutral-300/20 dark:border-neutral-300/50"
+				]
 			}
+
+			// {
+			// 	variant: "raised",
+			// 	color: "neutral",
+			// 	className: "text-white bg-gray-600 hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-800 dark:text-gray-800 dark:bg-gray-300 dark:hover:bg-gray-200 dark:focus:bg-gray-200 dark:active:bg-gray-100"
+			// }, {
+			// 	variant: "flat",
+			// 	color: "neutral",
+			// 	className: "text-gray-800 bg-gray-800/5 dark:text-gray-200 dark:bg-gray-200/5 hover:bg-gray-800/10 focus:bg-gray-800/15 active:bg-gray-800/20 dark:hover:bg-gray-200/10 dark:focus:bg-gray-200/15 dark:active:bg-gray-200/20"
+			// }, {
+			// 	variant: "outlined",
+			// 	color: "neutral",
+			// 	className: "border-gray-800/50 dark:border-gray-200/50"
+			// },
+
+			// {
+			// 	variant: "raised",
+			// 	disabled: true,
+			// 	className: "text-white bg-gray-500 shadow-none hover:shadow-none dark:text-white dark:bg-gray-500"
+			// }, {
+			// 	variant: "flat",
+			// 	disabled: true,
+			// 	className: "text-gray-700 dark:text-gray-200"
+			// }, {
+			// 	variant: "outlined",
+			// 	disabled: true,
+			// 	className: "text-gray-700 dark:text-gray-200 border-gray-500/50 dark:border-gray-400/50"
+			// }
 
 		]
 	}),

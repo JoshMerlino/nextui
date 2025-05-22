32dc3489f29f1b04b140aa63bd2d02dfc1526300
JoshMerlino <joshua.merlino@gmail.com>
2025-05-22 11:13:50 -0400

    Update glass variant in Toolbar component for enhanced backdrop blur effect

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -13,7 +13,7 @@ export const classes = {
 				false: "border-gray-200 dark:border-gray-800"
 			},
 			variant: {
-				glass: "backdrop-blur-xs bg-gray-100/60 dark:bg-gray-900/60"
+				glass: "backdrop-blur-2xl bg-gray-100/60 dark:bg-gray-900/60"
 			}
 		},
 		defaultVariants: {

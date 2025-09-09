a727c1862b7da9d95c050cac4c87157522127e17
Josh Merlino <joshua.merlino@gmail.com>
2025-09-09 13:58:29 -0400

    Update EqualGrid to count valid children for grid layout

--- util/EqualGrid.tsx
+++ util/EqualGrid.tsx
@@ -1,11 +1,17 @@
 import { type HTMLAttributes, type PropsWithChildren, Children } from "react";
 
 export function EqualGrid({ style, children, ...props }: Readonly<PropsWithChildren & HTMLAttributes<HTMLDivElement>>) {
+
+	// Count non null children
+	const validChildrenCount = Children.toArray(children).filter(child => child !== null && child !== undefined);
+
+	console.log({ validChildrenCount });
+
 	return (
 		<div { ...props }
 			style={{
 				...style,
-				gridTemplateColumns: `repeat(${ Children.count(children) }, minmax(0, 1fr))`
+				gridTemplateColumns: `repeat(${ Children.count(validChildrenCount) }, minmax(0, 1fr))`
 			}}>
 			{ children }
 		</div>

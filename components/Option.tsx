59d9c896ee81eabf51b410fd4ee09a2662aaccba
Josh Merlino <joshua.merlino@gmail.com>
2025-10-08 13:25:09 -0400

    Refactor Option component styles for improved focus and selection visibility

--- components/Option.tsx
+++ components/Option.tsx
@@ -20,10 +20,9 @@ export const Option = forwardRef<HTMLLIElement, HTMLAttributes<HTMLLIElement> & 
 				"min-h-10 px-4 flex items-center select-none relative overflow-hidden isolate not-motion-reduce:transition-colors duration-100 cursor-pointer",
 				"group-[.size-dense]/select:min-h-9 group-[.size-dense]/select:px-3 group-[.size-dense]/select:text-sm",
 				{
-					"bg-gray-200/50 dark:bg-gray-700/50": isFocused,
-					"bg-gray-200/75 dark:bg-gray-700/75": isSelected,
-					"active:bg-gray-200 dark:active:bg-gray-700": !isSelected,
-					"bg-gray-200 dark:bg-gray-700": isSelected && isFocused
+					"bg-gray-200/25 dark:bg-gray-700/50": isFocused,
+					"bg-gray-200/50 dark:bg-gray-700/75": isSelected,
+					"active:bg-gray-200/50 dark:active:bg-gray-700/75": !isSelected,
 				},
 				className
 			]) }
